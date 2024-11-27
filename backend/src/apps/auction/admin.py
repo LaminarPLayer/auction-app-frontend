d467964adfcab1616a9ff958c4e2a971b5a77e75
Piotr Grzegorczyk <peter.grzegorczyk@gmail.com>
2024-11-27 16:45:23 +0100

    remove redundant backend folder

--- backend/src/apps/auction/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from src.apps.auction.models import Auction, Bid
-
-# Register your models here.
-admin.site.register(Auction)
-admin.site.register(Bid)
--- backend/src/apps/auction/consts.py
+++
@@ -1,1 +0,0 @@
-BID_INCREMENT = 10
