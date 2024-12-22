3b7827a9404be30bfb9dbf9ae910e6b4e3bea335
Piotr Grzegorczyk <peter.grzegorczyk@gmail.com>
2024-12-22 21:30:52 +0100

    Remove PostHog integration and clean up layout structure
    - Deleted PostHogIdentify and PostHogPageView components to eliminate unnecessary tracking functionality.
    - Refactored layout structure in RootLayout component for improved readability and organization by removing the PHProvider.
    - Updated package.json and pnpm-lock.yaml to remove posthog-js dependency, streamlining the project and reducing bundle size.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,10 +6,8 @@ import { WelcomeModalProvider } from "@/lib/contexts/welcome-modal-context";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
-import PostHogIdentify from "./PostHogIdentify";
-import PostHogPageView from "./PostHogPageView";
 import "./globals.css";
-import { NextAuthProvider, PHProvider } from "./providers";
+import { NextAuthProvider } from "./providers";
 
 const rubik = Rubik({
   weight: ["400", "500", "700"],
@@ -32,30 +30,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <NextAuthProvider>
-        <PHProvider>
-          <WelcomeModalProvider>
-            <HomepageVisitedProvider>
-              <body className={`${rubik.className}`}>
-                <PostHogPageView />
-                <PostHogIdentify />
-                <div className="relative flex min-h-screen flex-col items-center">
-                  <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
-                  >
-                    <EdgeStoreProvider>
-                      <Navbar />
-                      <main className="w-full">{children}</main>
-                      <Toaster />
-                    </EdgeStoreProvider>
-                  </ThemeProvider>
-                </div>
-              </body>
-            </HomepageVisitedProvider>
-          </WelcomeModalProvider>
-        </PHProvider>
+        <WelcomeModalProvider>
+          <HomepageVisitedProvider>
+            <body className={`${rubik.className}`}>
+              <div className="relative flex min-h-screen flex-col items-center">
+                <ThemeProvider
+                  attribute="class"
+                  defaultTheme="system"
+                  enableSystem
+                  disableTransitionOnChange
+                >
+                  <EdgeStoreProvider>
+                    <Navbar />
+                    <main className="w-full">{children}</main>
+                    <Toaster />
+                  </EdgeStoreProvider>
+                </ThemeProvider>
+              </div>
+            </body>
+          </HomepageVisitedProvider>
+        </WelcomeModalProvider>
       </NextAuthProvider>
     </html>
   );
--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import posthog from "posthog-js";
-import { PostHogProvider } from "posthog-js/react";
-import { useEffect } from "react";
 
 type Props = {
   children?: React.ReactNode;
@@ -12,20 +9,3 @@ type Props = {
 export const NextAuthProvider = ({ children }: Props) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
-
-export function PHProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-    const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
-    if (posthogKey && posthogHost) {
-      posthog.init(posthogKey, {
-        api_host: posthogHost,
-        person_profiles: "always",
-        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-        capture_pageleave: true,
-      });
-    }
-  }, []);
-
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
-}
